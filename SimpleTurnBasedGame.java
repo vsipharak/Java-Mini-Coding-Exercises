4f4019c7cf59ee0ce5d6df1cc7deaad6aaa6232a
JayyJ <vasun.sipharak@gmail.com>
2024-12-11 03:51:14 -0500

    Code modification
    1. Added Max Number of roll attempts.
    2. Added Invalid Input message if user attempts more than 2 rolls.
    3. Modified code to ask user how many rolls they would like to play (max 2).
    4. Modified code to to show which player rolled and value of roll.

--- SimpleTurnBasedGame.java
+++ SimpleTurnBasedGame.java
@@ -4,6 +4,7 @@ public class SimpleTurnBasedGame
 {
     static Scanner input = new  Scanner(System.in); // now it can be seen every where can also make a copy and put it down witht the
     static Random random = new Random();
+
     public static void main(String[] args)
     {
         String playerOne = "Rabbit";
@@ -12,59 +13,78 @@ public class SimpleTurnBasedGame
         int playerTwoPosition = 0;
         int playerOneTurns = 0;
         int playerTwoTurns = 0;
-        int playerRollValue = 0;
 
-        while(true)
-        {
-            playerRollValue = rollDice();
+        while(true) {
+            // Player 1's turn
+            System.out.println(playerOne + ", how many times would you like to roll the die? (Max: 2)");
+            int playerOneRolls = getValidRolls();
+            int playerOneRollValue = 0;
+            for (int i = 0; i < playerOneRolls; i++) {
+                int[] playerOneDice = rollDice();
+                playerOneRollValue = playerOneDice[0] + playerOneDice[1]; //Calculates sum of dice
+                System.out.println(playerOne + " rolled die 1: " + playerOneDice[0] + " and die 2: " + playerOneDice[1] + ".");
+                playerOnePosition += playerOneRollValue;
+                System.out.println(playerOne + " is now at position " + playerOnePosition);
+                if (playerOnePosition >= 100) {
+                    System.out.println("The " + playerOne + " has won!");
+                    System.out.println("Number of turns: " + playerOneTurns);
+                    System.exit(0);
+                }
+            }
             playerOneTurns++;
-            playerOnePosition = playerOnePosition + playerRollValue;
-            if(playerOnePosition >= 100)
-            {
-                System.out.println("The " + playerOne + " has won");
-                System.out.println("Number of turns " + playerOneTurns);
-                System.exit(0);
-            }
-            if(playerOnePosition == playerTwoPosition)
-            {
+
+            if (playerOnePosition == playerTwoPosition) {
                 System.out.println("The " + playerOne + " landed on the " + playerTwo);
-                System.out.println("Good bye " + playerTwo);
+                System.out.println("Goodbye, " + playerTwo + ". Best of luck next time!");
                 playerTwoPosition = 0;
-
             }
 
-
-            playerRollValue = rollDice();
+            // Player 2's Turn
+            System.out.println(playerTwo + ", how many times would you like to roll the die? (Max: 2)");
+            int playerTwoRolls = getValidRolls();
+            int playerTwoRollValue = 0;
+            for (int i = 0; i < playerTwoRolls; i++) {
+                int[] playerTwoDice = rollDice();
+                playerTwoRollValue = playerTwoDice[0] + playerTwoDice[1]; //Calculates sum of dice
+                System.out.println(playerTwo + "rolled die 1: " + playerTwoDice[0] + " and die 2: " + playerTwoDice[1] + ".");
+                playerTwoPosition += playerTwoRollValue;
+                System.out.println(playerTwo + " is now at position " + playerTwoPosition);
+                if (playerTwoPosition >= 100) {
+                    System.out.println("The " + playerTwo + " has won!");
+                    System.out.println("Number of turns: " + playerTwoTurns);
+                    System.exit(0);
+                }
+            }
             playerTwoTurns++;
-            playerTwoPosition = playerTwoPosition + playerRollValue;
-            if(playerTwoPosition >= 100)
-            {
-                System.out.println("The " + playerTwo + " has won");
-                System.out.println("Number of turns " + playerTwoTurns);
-                System.exit(0);
-            }
 
-            if(playerTwoPosition == playerOnePosition)
-            {
+            if (playerTwoPosition == playerOnePosition) {
                 System.out.println("The " + playerTwo + " landed on the " + playerOne);
-                System.out.println("Good bye " + playerOne);
+                System.out.println("Goodbye, " + playerOne + ". Best of luck next time!");
                 playerOnePosition = 0;
-
             }
-
         }
-    } // end of main
+    }
+        // end of main
 
-    public static int rollDice()
+        //Modified rollDice method to return both die rolls
+    public static int[] rollDice()
     {
-
         int die1 = 1 + random.nextInt(6); // first die roll
         int die2 = 1 + random.nextInt(6); // second die roll
-        int sum = die1 + die2; // sum of die values
-        // display results of this roll
-        System.out.printf("Player rolled %d + %d = %d%n", die1, die2, sum);
+        // Return an array with both die values
+        return new int[] {die1, die2};
+    }
 
-        return sum;
+        // Added method to get a valid number of rolls (1 or 2)
+    public static int getValidRolls()
+    {
+        int rolls = input.nextInt();
+        while (rolls < 1 || rolls > 2)
+        {
+            System.out.println("Invalid input! You can only choose '1' or '2' rolls. Please try again. :)");
+            rolls = input.nextInt();
+        }
+        return rolls;
     }
     // end class
 }
